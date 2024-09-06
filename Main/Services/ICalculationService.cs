9e1dbc6c03f0d021b730623aa0cafa36ed47efd3
Григорий Алексеев <burtelgamerpro@gmail.com>
2024-09-06 17:08:43 +0600

    Доработка интерфейса

--- Main/Services/ICalculationService.cs
+++ Main/Services/ICalculationService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Main.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace Main.Services
 {
     internal interface ICalculationService
     {
+        CalculationResult Calculate(string productId, IEnumerable<SupplierOffer> supplierOffers, IEnumerable<StockItem> stockItems);
     }
 }
